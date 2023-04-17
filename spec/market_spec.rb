57c2669e937089be1c2c79a01d97753134bfa575
Michael Callahan <mcalla242@gmail.com>
2023-04-17 09:37:27 -0600

    Test: Add testing specs for Market class

---
+++ spec/market_spec.rb
@@ -0,0 +1,62 @@
+require 'rspec'
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+RSpec.describe Market do
+  before(:each) do
+    @market = Market.new("South Pearl Street Farmers Market") 
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor1.stock(item1, 35)
+    @vendor1.stock(item2, 7)
+    @vendor2.stock(item4, 50)
+    @vendor2.stock(item3, 25)
+    @vendor3.stock(item1, 65)
+  end
+
+  describe "#initialize" do
+    it "can initialize" do
+      expect(@market).to be_an_instance_of(Market)
+    end
+
+    it "can initialize with attributes" do
+      expect(@market.name).to eq("South Pearl Street Farmers Market")
+      expect(@market.vendors).to eq([])
+    end
+  end
+
+  describe "#add_vendor" do
+    it "can add a vendor" do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
+      expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
+    end
+  end
+
+  before(:each) do
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+  end
+
+  describe "#vendor_names" do
+    it "return a list of the vendors names" do
+      expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+  end
+
+  describe "#vendors_that_sell" do
+    it "can return a list of Vendors that sell a certain item" do
+      expect(@market.vendors_that_sell(@item1))
+      expect(@market.vendors_that_sell(@item4))
+    end
+  end
+end
